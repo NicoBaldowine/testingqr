79ea2971a90fdb45e97067c96c511a6f333bdf0d
Nico Baldovino <nicobaldovino@Nicos-Laptop.local>
2024-09-09 13:29:24 -0600

    Fix folder structure for Next.js App Router

---
+++ app/my-quizzes/my-quizzes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyQuizzes = () => {
+  return (
+    <div>
+      <h1>My Quizzes</h1>
+      <p>This is where you'll see all your quizzes.</p>
+    </div>
+  );
+};
+
+export default MyQuizzes;
---
+++ app/ranking/ranking.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Ranking = () => {
+  return (
+    <div>
+      <h1>Ranking</h1>
+      <p>This is where you'll see the rankings of users.</p>
+    </div>
+  );
+};
+
+export default Ranking;
